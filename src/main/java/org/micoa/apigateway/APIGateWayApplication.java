1e8fb46f3a5d4dd52c7c197918c36048cdd03a16
liuzhenyuan <liuzhenyuan@meituan.com>
2016-08-17 17:04:09 +0800

    add

--- src/main/java/org/micoa/apigateway/APIGateWayApplication.java
+++ src/main/java/org/micoa/apigateway/APIGateWayApplication.java
@@ -6,8 +6,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
-@EnableDiscoveryClient
-@EnableZuulProxy
+//@EnableDiscoveryClient
+//@EnableZuulProxy
 public class APIGateWayApplication {
 
 	public static void main(String[] args) {
